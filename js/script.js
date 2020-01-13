1277e7331818b2e5f9aea7670124d13397390570
Mustav Hamraz <mustavhamraz@Mustavs-MacBook-Pro.local>
2020-01-13 09:08:27 +0100

    function for empty letters has been done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-///*****     Login Section     *****///
-//keeps everything hidden until there is a user name given
+//              ***********     LOGIN SECTION     ***********
+// Håller allt döld till användaren skriver ett namn
 let loginSection = document.getElementById('login');
 let helpSection = document.getElementById('help-section');
 let instructionSection = document.getElementById('instraction-section');
@@ -8,8 +8,7 @@ helpSection.style.display = "none";
 instructionSection.style.display = "none";
 gameSection.style.display = "none";
 
-
-//submites player's name by pushing EnterKey
+// Submites spelarens namn med Enter knappen
 let userName = document.getElementById('nameinput');
 userName.addEventListener(
     "keyup", function(event){
@@ -20,8 +19,8 @@ userName.addEventListener(
     }
 );
 
-//function which saves Player's name
-function greetings(){
+
+function greetings(){                       // Funktion som sparar spelarens namn
     userName = document.getElementById('nameinput').value; //gets player's input
     
     if(userName.length > 0){
@@ -36,11 +35,7 @@ function greetings(){
 
 
 
-
-
-
-
-///*****     Help Section     *****///
+//              ***********     HELP SECTION     ***********
 function yesFunction(){
     helpSection.style.display = "none";
     instructionSection.style.display = "block";
@@ -54,12 +49,33 @@ function noFunction(){
 
 
 
-
-
-
-
-///*****     Instraction Section     *****///
+//              ***********     INSTRACTION SECTION     ***********
 function startGame(){
     instructionSection.style.display = "none";
     gameSection.style.display = "block";
+    hidenWord(); //ropar på funktionen som skapar toma rutor
 }
+
+
+
+
+
+//               ***********     MAIN GAME SECTION     ***********
+
+const wordList = ["finland", "italien", "tyskland", "spanien", "polen", "lettland"];    // Array med spelets alla ord
+let selectedWord = wordList[Math.floor(Math.random() * wordList.length)]                // Slumpgenerator från arrayen ovan
+
+
+function hidenWord(){                                                     // Funktion som skapar rutor beroande på antal bokstäverna
+    for(i =0; i < selectedWord.length; i++){
+        let guessList = document.createElement("LI");                     // Skapar en <li> node
+        let textGuessLetter = document.createTextNode('🔒');               // Skapar en text node
+        guessList.appendChild(textGuessLetter);                           // Appenderar texten i <li>
+        document.getElementById("guess-list").appendChild(guessList);     // Appenderar <li> i HTML
+        document.getElementById("guess-list").style.fontSize = "50px";    // Ändrar text storlek
+        document.getElementById("guess-list").style.color = "white";      // Ändrar text färg
+    }
+} 
+   
+// Håller antalet gissningar som gjorts
+let guesses = 0;
